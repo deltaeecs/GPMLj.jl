2898916111d71bae6ca4d6205213d85a6d243b8d
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-01-07 13:24:35 +0530

    modify travis setup
    modify travis setup
    
    modify travis setup
    
    modify travis setup

--- deps/build.jl
+++ deps/build.jl
@@ -3,7 +3,7 @@ using PyCall
 println("Running build.jl for the GPJ package.")
 
 # Change that to whatever packages you need.
-const PACKAGES = ["gpflow==1.5.1"]
+const PACKAGES = ["gpflow==1.5.1", "tensorflow==1.15.0"]
 
 # Use eventual proxy info
 proxy_arg=String[]
